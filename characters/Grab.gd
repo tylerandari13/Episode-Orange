cb0ac576349dad30dd54f92745b213c4da96e5d7
Prune Juice <118036786+tylerandari13@users.noreply.github.com>
2023-11-03 07:24:35 -0500

    no speed loss when mach grabbing

--- characters/Grab.gd
+++ characters/Grab.gd
@@ -10,6 +10,8 @@ var dir : float = 1
 var grab_time = 0.7
 var cur_time = 0
 
+var grab_velocity : float = 0
+
 func state_input(_event : InputEvent):
 	pass
 
@@ -20,14 +22,15 @@ func state_process(delta):
 		character.velocity.x = 0
 		character.animated_sprite.play("default")
 		if(Input.is_action_pressed("run")):
-			mach_state.override_speed(mach_state.mach3)
+			mach_state.override_speed(grab_velocity)
 			next_state = mach_state
 		else:
 			next_state = ground_state
 
 func on_enter():
 	cur_time = 0
-	character.velocity.x = (mach_state.mach3 if character.velocity.abs().x <= mach_state.mach3 else character.velocity.abs().x) * dir
+	grab_velocity = (mach_state.mach3 if character.velocity.abs().x <= mach_state.mach3 else character.velocity.abs().x)
+	character.velocity.x = grab_velocity * dir
 	character.animated_sprite.play("grab")
 
 func on_exit():
