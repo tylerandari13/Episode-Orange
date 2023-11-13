5085215ef48bfe7a0e7ad789504e64a538bab5b9
Prune Juice <tylerandari12@gmail.com>
2023-11-13 02:39:21 -0600

    very smalll cleanup

--- characters/Room_Changer.gd
+++ characters/Room_Changer.gd
@@ -1,13 +1,4 @@
 extends PlayerTrigger
 
-@export var room_name : String
-@export var room_spawnpoint : String
-
 func collision_entered(player : CharacterBody2D):
 	player.change_room(room_name, room_spawnpoint)
-
-func collision_exited(_player : CharacterBody2D):
-	pass
-
-func collision_process(_player : CharacterBody2D, _delta : float):
-	pass
