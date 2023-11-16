aa4327292e813078e88cf3d696a0daf7b2055e49
Prune Juice <tylerandari12@gmail.com>
2023-11-16 16:08:21 -0600

    add `add_player` (i forgot why tho)

--- characters/playerspawner.gd
+++ characters/playerspawner.gd
@@ -10,3 +10,9 @@ func spawn_player(name : String, parent : Node = self):
 	container.name = name
 	parent.add_child(container)
 	return container
+
+func get_player(name : String):
+	if(get_node(name)):
+		for child in get_node(name).get_children():
+			if(child is Player):
+				return child
--- scripts/Level.gd
+++ scripts/Level.gd
@@ -6,10 +6,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player_holder = Players.spawn_player("orange")
+	if(Players.get_player("orange") == null):
+		Players.spawn_player("orange")
+	var player_holder = Players.get_player("orange")
 	if(!start_room.is_empty() || !start_spawn.is_empty()):
-		player_holder.get_node("orange").change_room("main" if start_room.is_empty() else start_room,
-													"main" if start_spawn.is_empty() else start_spawn)
+		player_holder.change_room("main" if start_room.is_empty() else start_room,
+								"main" if start_spawn.is_empty() else start_spawn)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
