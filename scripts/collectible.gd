73e563dbf12bf1ac26c80dd7a092f29f5d4e16c1
Prune Juice <tylerandari12@gmail.com>
2023-11-20 00:12:05 -0600

    Stuff for collectibles.

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -3,15 +3,15 @@ extends PlayerTrigger
 
 var respawns = false
 var alpha = modulate.a
+var collected = false
 
 func collision_entered(player : CharacterBody2D):
+	modulate.a = alpha * 0.5 if player.get_current_room().is_secret else 0
 	collect(player)
 
 func respawn():
-	visible = true
 	modulate.a = alpha
+	collected = false
 
 func collect(player : CharacterBody2D):
-	if(player.get_current_room().is_secret):
-		modulate.a = 0.5
-	visible = false
+	collected = true
