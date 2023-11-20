c67d6bb0d66a331cbd775afe4edc1366dd62f491
Prune Juice <tylerandari12@gmail.com>
2023-11-19 23:18:20 -0600

    How did i fumpbe this badly?
    Added `RoomChanger.gd`, because somehow i put the original roomchanger code into the main playertrigger class.

--- scripts/PlayerTrigger.gd
+++ scripts/PlayerTrigger.gd
@@ -3,25 +3,28 @@ extends Area2D
 
 var has_called : bool = false
 
-@export var room_name : String = "main"
-@export var room_spawnpoint : String = "main"
+func _ready():
+	body_entered.connect(self._on_body_entered)
+	body_exited.connect(self._on_body_exited)
 
 func _process(delta):
 	for body in get_overlapping_bodies():
-		if("is_player" in body):
+		if("is_player" in body || body is Player):
 			collision_process(body, delta)
 
 func _on_body_entered(body):
-	if("is_player" in body):
+	print("is_player" in body)
+	print(body is Player)
+	if("is_player" in body || body is Player):
 		collision_entered(body)
 
 func _on_body_exited(body):
-	if("is_player" in body):
+	if("is_player" in body || body is Player):
 		collision_exited(body)
 
 
 func collision_entered(player : CharacterBody2D):
-	player.change_room(room_name, room_spawnpoint)
+	pass
 
 func collision_exited(_player : CharacterBody2D):
 	pass
---
+++ scripts/RoomChanger.gd
@@ -0,0 +1,9 @@
+class_name RoomSwitcher
+extends PlayerTrigger
+
+@export var room_name : String = "main"
+@export var room_spawnpoint : String = "main"
+
+func collision_entered(player : CharacterBody2D):
+	print("GUH?")
+	player.change_room(room_name, room_spawnpoint)
