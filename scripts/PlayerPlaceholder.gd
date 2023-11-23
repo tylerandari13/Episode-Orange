b7c044be9fc6ba436f6011b7825445102c0e3c0e
Prune Juice <tylerandari12@gmail.com>
2023-11-23 15:12:04 -0600

    Set z pos of items to multiples of 100 instead of 1

--- scripts/PlayerPlaceholder.gd
+++ scripts/PlayerPlaceholder.gd
@@ -6,8 +6,8 @@ func _ready():
 	for child in get_parent().get_children():
 		if(child is Node2D && child.z_index == 0):
 			if(passed_self):
-				child.z_index = 1
+				child.z_index = 100
 			else:
-				child.z_index = -1
+				child.z_index = -100
 		if(child == self):
 			passed_self = true
