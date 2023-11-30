d580e492a166b0d3d60b47517515b7c9356b490d
Prune Juice <tylerandari12@gmail.com>
2023-11-30 16:11:34 -0600

    add the token collectible

--- scripts/collectible_small.gd
+++ scripts/collectible_small.gd
@@ -2,7 +2,8 @@ class_name CollectibleSmall
 extends CollectibleBase
 
 func set_texturepaths(): return [
-	 "res://images/objects/colllectibles/ticket_small/ticket_small.tres"
+	 "res://images/objects/colllectibles/ticket_small/ticket_small.tres",
+	"res://images/objects/colllectibles/token_small/token_small.tres",
 ]
 
 func on_collected(player : CharacterBody2D):
