2f82d6694c25742dc76fbebd30f9ad3f3683a53a
Prune Juice <tylerandari12@gmail.com>
2023-12-02 14:59:52 -0600

    remove annoying print message

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -5,7 +5,6 @@ var rooms : Dictionary
 var collectibles : Array
 
 func add_room(room : Room):
-	print("added room ", room)
 	rooms[room.name] = room
 func get_room(room :String):
 	return rooms[room]
