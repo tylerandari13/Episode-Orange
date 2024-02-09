b8e845fd376071db9c87c85d18a7087c7e122374
Prune Juice <118036786+tylerandari13@users.noreply.github.com>
2024-02-09 00:39:58 -0600

    im never using tokens again

--- scripts/blocks/block.gd
+++ scripts/blocks/block.gd
@@ -2,30 +2,26 @@ class_name Block
 extends StaticBody2D
 
 @export var block_damage : int
+@export var sprite : AnimatedSprite2D
 
 @onready var collision = $Area2D
 
+var broken = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	collision.body_entered.connect(_touched)
+	collision.body_entered.connect(touch)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _touched(body : Node2D):
+func touch(body : Node2D):
 	if(body is Player):
-		if(body.get_block_damage() >= block_damage):
+		if(body.get_block_damage() >= block_damage && _on_touched(body)):
+			broken = true
 			collision_layer = 0
-			visible = false
-	#	print(body.get_block_damage(), " : ", body.get_mach_speed())
-	
+			sprite.modulate.a = 0
 
-
-
-
-
-
-
-
+func _on_touched(player : Player) -> bool: return true
---
+++ scripts/blocks/point_block.gd
@@ -0,0 +1,19 @@
+class_name PointBlock
+extends Block
+
+@export var worth = 0
+@export var point_text : Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+	point_text.text = str(worth)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta): if(broken): point_text.position.y -= 5
+
+func _on_touched(player : Player) -> bool:
+	player.add_points(worth)
+	point_text.visible = true
+	return true
