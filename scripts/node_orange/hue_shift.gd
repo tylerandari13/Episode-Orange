32b242f0f7ab5714b5737cda20031ef7db16fc98
Prune Juice <tylerandari12@gmail.com>
2024-02-11 23:55:03 -0600

    guh

--- scripts/node_orange/hue_shift.gd
+++ scripts/node_orange/hue_shift.gd
@@ -8,12 +8,15 @@ extends NodeOrange
 @export var divide_by = 10
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready(): set_hue()
+
+func set_hue(hue = NAN):
+	if(is_nan(hue)): hue = shift
 	if(!is_instance_valid(target)): target = get_parent()
 	if(target is CanvasItem):
 		if(random):
 			target.material = Global.hue_shader(randi_range(1, divide_by) / float(divide_by))
 		else:
-			target.material = Global.hue_shader(shift)
+			target.material = Global.hue_shader(hue)
 	else:
 		push_warning("Node ", target, " is not a child of CanvasItem.")
