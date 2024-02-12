f9f258df36afaee62f9a34dddb6a105e04a361e0
Prune Juice <tylerandari12@gmail.com>
2024-02-11 21:38:14 -0600

    Optomize hue shaders.

--- scripts/collectibles/collectible_randomhue.gd
+++ scripts/collectibles/collectible_randomhue.gd
@@ -4,5 +4,4 @@ extends Collectible
 
 # Called when the node enters the scene tree for the first time.
 func ready():
-	sprite.material = sprite.material.duplicate(true)
-	sprite.material.set_shader_parameter("Shift_Hue", (randi() % 10) * 0.1)
+	sprite.material = Global.hue_shader((randi() % 10) * 0.1)
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,4 +1,15 @@
-extends Node
+extends NodeOrange
+
+var hue_shaders = {}
+var hue_shader_placeholder = preload("res://shader/hue_shift.gdshader")
 
 # do not change the 60 lmao
 func apply_delta_time(start, delta): return (start * delta) * 60
+
+func hue_shader(hue):
+	if(!(hue in hue_shaders)):
+		var new_shader = ShaderMaterial.new()
+		new_shader.shader = hue_shader_placeholder.duplicate(true)
+		new_shader.set_shader_parameter("Shift_Hue", hue)
+		hue_shaders[hue] = new_shader
+	return hue_shaders[hue]
