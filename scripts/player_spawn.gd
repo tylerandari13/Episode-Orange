1fb0ef2c3fce71fa3b98d43d5f447ccb3ed3eff4
Prune Juice <tylerandari12@gmail.com>
2024-02-18 00:31:53 -0600

    Temporary player spawn point.

---
+++ scripts/player_spawn.gd
@@ -0,0 +1,8 @@
+class_name PlayerSpawn
+extends Marker2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite2D.visible = false
+	add_child(load("res://scenes/players/orange.tscn").instantiate())
