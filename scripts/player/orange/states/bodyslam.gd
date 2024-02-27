4a85e61d23af392016a10fd90e543f439b08a3ce
Prune Juice <tylerandari12@gmail.com>
2024-02-27 07:44:08 -0600

    slight bodyslam tweaks

--- scripts/player/orange/states/bodyslam.gd
+++ scripts/player/orange/states/bodyslam.gd
@@ -17,7 +17,17 @@ func on_physics_process(delta):
 	owner.velocity.y += owner.gravity * delta # this on top of the "use gravity" doubles it
 
 	if(owner.is_on_floor()):
-		change_state("none/bonk")
+		#print(int(rad_to_deg(owner.get_floor_normal().angle()) + 90))
+		#print(owner.get_floor_normal().x)
+		#owner.find_child("RayCast2D").target_position = owner.get_floor_normal() * 150
+		if(abs(int(rad_to_deg(owner.get_floor_normal().angle()) + 90)) <= 15):
+			change_state("none/bonk")
+		else:
+			owner.direction = -1 if owner.get_floor_normal().x < 0 else 1
+		#	owner.mach_speed = owner.mach3 / owner.get_position_delta().y * 5
+		#	print(owner.mach_speed)
+		#	owner.mach_speed = min(owner.mach_speed, owner.mach3)
+			change_state("none/machrun")
 
 	if(Input.is_action_pressed("left") && owner.velocity.x > owner.walk_speed * -0.5): owner.velocity.x -= move_velocity
 	if(Input.is_action_pressed("right") && owner.velocity.x < owner.walk_speed * 0.5): owner.velocity.x += move_velocity
